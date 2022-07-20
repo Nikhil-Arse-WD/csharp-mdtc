3c79babda2593fd471372fc89930c012654f0d9c
Nikhil-Arse-WD <103878716+Nikhil-Arse-WD@users.noreply.github.com>
2022-07-20 13:42:13 +0530

    Add files via upload

---
+++ MDTCsol/MDTC/Models/Calc.cs
@@ -0,0 +1,24 @@
+﻿namespace MDTC.Models
+{
+    public class Calc
+    {
+       public double USDRate { get; set; }
+       public double Vgrt { get; set; }
+
+       public double BStay { get; set; }
+       
+       public double Pdues { get; set; }
+
+       public double Pilot { get; set; }
+
+       public double Bhire  { get; set; }
+
+       public double  Anchor { get; set; }
+
+       public double TugT { get; set; }
+     
+       public double TugP { get; set; }
+
+       
+    }
+}
